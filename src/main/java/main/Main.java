ac16bab27fd92a6d72cfc937a5fed06213d6bb95
Maja Barwińska <majakempa1@gmail.com>
2019-10-19 16:03:57 +0200

    FluentBuilder

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,17 +8,21 @@ public class Main {
     public static void main(String[] args) {
         AuctionNotifier auctionNotifier = new AuctionNotifierImplementation();
 
-        Customer seller = new Customer("Jan Kowalski");
+/*        Customer seller = new Customer("Jan Kowalski");
         Auction auction = new Auction(1,"Coffee",null,
                 LocalDateTime.now().plusDays(2L),seller,0,null);
+        */
+        Auction auction = Auction.builder().withId(1).withItem("Camera").withDescription("Super camera!")
+                .withSeller(new Customer("Jan Kowalski")).build();
+        System.out.println(auction);
 
 
         AuctionService auctionService = AuctionService.getInstance();
         Customer customer1 = new Customer("Anna Kowalik");
-        auctionService.bid(auction,customer1,20);
+        auctionService.bid(auction, customer1, 20);
         auctionNotifier.registerObserver(customer1);
         Customer customer2 = new Customer("Krzysztof Adamowicz");
-        auctionService.bid(auction,customer2,25);
+        auctionService.bid(auction, customer2, 25);
         auctionNotifier.registerObserver(customer2);
         auctionNotifier.notifyObservers();
 
